bbd3a3dc585d4e3ec7697c35ee10ec95488db3de
zhengmin317 <zhengmin317@gmail.com>
2016-01-27 15:56:33 +0100

    updated javadoc

--- yoink-core-api/src/main/java/org/wallerlab/yoink/api/model/bootstrap/JobParameter.java
+++ yoink-core-api/src/main/java/org/wallerlab/yoink/api/model/bootstrap/JobParameter.java
@@ -16,45 +16,44 @@
 package org.wallerlab.yoink.api.model.bootstrap;
 
 /**
- * JobParameter is used to describe the type of parameter in parameters of job
- * {@link org.wallerlab.yoink.api.model.bootstrap.Job}.
+ * <p>JobParameter is used to describe the type of parameter in parameters of job
+ * {@link org.wallerlab.yoink.api.model.bootstrap.Job}.</p>
  * 	
- * *
-	PARTITIONER: the partitioner type DISTANCE/NUMBER/DORI
-	OUTPUT_FOLDER: the folder for out put files
-	JOB_NAME: the name of the job, and the prefix of the result file
- * parameters for distance and number-based partitioning 
-    NUMBER_QM: number of qm molecules in number-based partitioning
-	NUMBER_BUFFER: number of buffer(transition) molecules in number-based partitioning
-	DISTANCE_QM: number of  qm molecules in distance-based partitioning
-	DISTANCE_BUFFER: number of buffer(transition) molecules in distance-based partitioning 
- * parameters for density-based partitioning
-	DORI: DORI threshold in density-based partitioning
-	SEDD: SEDD threshold in density-based partitioning
-	DENSITY_SEDD: density threshold in SEDD analysis
-	DENSITY_DORI: density threshold in DORI analysis
-	DENSITY_ASR_QMCORE: density threshold of adaptive search region for adaptive qm core (SEDD analysis)
-	DENSITY_ASR_QM:  density threshold of adaptive search region for adaptive qm (DORI analysis)
-	DENSITY_RATIO_MIN: the minimum of density ratio of two atoms having inter-atomic interaction
-	DENSITY_RATIO_MAX: the maximum of density ratio of two atoms having inter-atomic interaction 
-	SEDD_STEPSIZE: the step size of the cube in SEDD analysis.
-	DORI_STEPSIZE:  the step size of the cube in DORI analysis.
-	REGION_CUBE: the region to construct a cube for the DORI/SEDD calculation. The suggested one is ADAPTIVE_SEARCH.
-	DENSITY_BUFFER: the density threshold for the buffer(transition) region in density-based partitioning
-	DENSITY_QM: the density threshold for the QM region in density-based partitioning, used in smoothing function.
- *smoothing 
-	SMOOTHNER: the smoothing type.
- ** smoothing DISTANCE_SCMP
-	DISTANCE_S_QM_IN: distance threshold for QM molecules into QM region
-	DISTANCE_T_QM_IN: distance threshold for QM molecules into transition region
-	DISTANCE_S_QM_OU: distance threshold for QM molecules out of QM region
-	DISTANCE_T_QM_OUT: distance threshold for QM molecules out of transition
-	DISTANCE_S_MM_IN:  distance threshold for MM molecules into QM region
-	DISTANCE_T_MM_IN: distance threshold for MM molecules into transition region
-	DISTANCE_S_MM_OUT: distance threshold for MM molecules  out of QM region
-	DISTANCE_T_MM_OUT: distance threshold for MM molecules out of transition
-	NUMBER_PARTITION: number of partitioning in DISTANCE_SCMP
-	
+ * 
+ *<p>  PARTITIONER: the partitioner type DISTANCE/NUMBER/DORI</p>
+ *<p>  OUTPUT_FOLDER: the folder for out put files</p>
+ *<p>	JOB_NAME: the name of the job, and the prefix of the result file</p>
+ *<p> parameters for distance and number-based partitioning </p>
+ *<p>  NUMBER_QM: number of qm molecules in number-based partitioning</p>
+ *<p>	NUMBER_BUFFER: number of buffer(transition) molecules in number-based partitioning</p>
+ *<p>	DISTANCE_QM: number of  qm molecules in distance-based partitioning</p>
+ *<p>	DISTANCE_BUFFER: number of buffer(transition) molecules in distance-based partitioning </p>
+ *<p> parameters for density-based partitioning</p>
+ *<p>	DORI: DORI threshold in density-based partitioning</p>
+ *<p>	SEDD: SEDD threshold in density-based partitioning</p>
+ *<p>	DENSITY_SEDD: density threshold in SEDD analysis</p>
+ *<p>	DENSITY_DORI: density threshold in DORI analysis</p>
+ *<p>	DENSITY_ASR_QMCORE: density threshold of adaptive search region for adaptive qm core (SEDD analysis)</p>
+ *<p>	DENSITY_ASR_QM:  density threshold of adaptive search region for adaptive qm (DORI analysis)</p>
+ *<p>	DENSITY_RATIO_MIN: the minimum of density ratio of two atoms having inter-atomic interaction</p>
+ *<p>	DENSITY_RATIO_MAX: the maximum of density ratio of two atoms having inter-atomic interaction </p>
+ *<p>	SEDD_STEPSIZE: the step size of the cube in SEDD analysis.</p>
+ *<p>	DORI_STEPSIZE:  the step size of the cube in DORI analysis.</p>
+ *<p>	REGION_CUBE: the region to construct a cube for the DORI/SEDD calculation. The suggested one is ADAPTIVE_SEARCH.</p>
+ *<p>	DENSITY_BUFFER: the density threshold for the buffer(transition) region in density-based partitioning</p>
+ *<p>	DENSITY_QM: the density threshold for the QM region in density-based partitioning, used in smoothing function.</p>
+ *<p>smoothing </p>
+ *<p>	SMOOTHNER: the smoothing type.</p>
+ *<p>smoothing DISTANCE_SCMP</p>
+ *<p>	DISTANCE_S_QM_IN: distance threshold for QM molecules into QM region</p>
+ *<p>	DISTANCE_T_QM_IN: distance threshold for QM molecules into transition region</p>
+ *<p>	DISTANCE_S_QM_OU: distance threshold for QM molecules out of QM region</p>
+ *<p>	DISTANCE_T_QM_OUT: distance threshold for QM molecules out of transition</p>
+ *<p>	DISTANCE_S_MM_IN:  distance threshold for MM molecules into QM region</p>
+ *<p>	DISTANCE_T_MM_IN: distance threshold for MM molecules into transition region</p>
+ *<p>	DISTANCE_S_MM_OUT: distance threshold for MM molecules  out of QM region</p>
+ *<p>	DISTANCE_T_MM_OUT: distance threshold for MM molecules out of transition</p>
+ *<p>	NUMBER_PARTITION: number of partitioning in DISTANCE_SCMP	</p>
  * @author Min Zheng
  *
  */
