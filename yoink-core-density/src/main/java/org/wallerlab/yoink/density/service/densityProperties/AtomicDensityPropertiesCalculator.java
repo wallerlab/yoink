d4a4155327cc6c66fe466ce34f2241e3b7851c8f
zhengmin317 <zhengmin317@gmail.com>
2016-06-16 11:36:49 +0200

    updated dgrid density calculation

--- yoink-core-density/src/main/java/org/wallerlab/yoink/density/service/densityProperties/AtomicDensityPropertiesCalculator.java
+++ yoink-core-density/src/main/java/org/wallerlab/yoink/density/service/densityProperties/AtomicDensityPropertiesCalculator.java
@@ -16,17 +16,20 @@
 package org.wallerlab.yoink.density.service.densityProperties;
 
 import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 import org.wallerlab.yoink.api.model.density.DensityPoint;
 import org.wallerlab.yoink.api.model.molecular.Atom;
 import org.wallerlab.yoink.api.model.molecular.Coord;
 import org.wallerlab.yoink.api.model.molecular.Element;
+import org.wallerlab.yoink.api.model.molecular.RadialGrid;
 import org.wallerlab.yoink.api.service.Calculator;
 import org.wallerlab.yoink.api.service.math.Matrix;
 import org.wallerlab.yoink.api.service.math.Vector;
 import org.wallerlab.yoink.math.constants.Constants;
 import org.wallerlab.yoink.math.linear.CommonsMatrix;
 import org.wallerlab.yoink.math.linear.SimpleMatrixFactory;
+
 /**
  * This class is to calculate the Properties of a density point from an atom.
  * 
@@ -38,6 +41,7 @@ public class AtomicDensityPropertiesCalculator implements
 		Calculator<DensityPoint, DensityPoint, Atom> {
 	@Resource
 	private SimpleMatrixFactory myMatrix;
+
 	/**
 	 * calculate a density point's properties from an atom
 	 * 
@@ -51,21 +55,10 @@ public class AtomicDensityPropertiesCalculator implements
 	public DensityPoint calculate(DensityPoint densityPoint, Atom atom) {
 		// initialize
 		Vector distanceVector = getDistanceVectorAndAtomName(densityPoint, atom);
-		Element atomName = atom.getElementType();
-		double distance = Math.max(distanceVector.getNorm(),
-				Constants.DISTANCE_DEFAULT);
-		double distanceReciprocal = 1.0 / distance;
-		Vector distanceUnitVector = distanceVector
-				.scalarMultiply(distanceReciprocal);
-		double exp1 = Math.exp(-distance / atomName.z1());
-		double exp2 = Math.exp(-distance / atomName.z2());
-		double exp3 = Math.exp(-distance / atomName.z3());
-		double fac1 = atomName.cz1() * exp1 + atomName.cz2() * exp2
-				+ atomName.cz3() * exp3;// first derivative of density
-		//get the values of densityPoint
+		
+		// get the values of densityPoint
 		densityPoint = wrapDensityBasedProperties(densityPoint, distanceVector,
-				atomName, distanceReciprocal, distanceUnitVector, exp1, exp2,
-				exp3, fac1);
+				atom);
 		return densityPoint;
 	}
 
@@ -79,13 +72,90 @@ public class AtomicDensityPropertiesCalculator implements
 	}
 
 	private DensityPoint wrapDensityBasedProperties(DensityPoint densityPoint,
-			Vector distanceVector, Element atomName, double distanceReciprocal,
-			Vector distanceUnitVector, double exp1, double exp2, double exp3,
-			double fac1) {
+			Vector distanceVector, Atom atom) {
+		Element atomName = atom.getElementType();
+		double distance = Math.max(distanceVector.getNorm(),
+				Constants.DISTANCE_DEFAULT);
+		double distanceReciprocal = 1.0 / distance;
+		Vector distanceUnitVector = distanceVector
+				.scalarMultiply(distanceReciprocal);
+		double fac1 = 0;
+		double fac2 = 0;
+		double density = 0;
+		if(atom.getRadialGrid()==null){
+		double exp1 = Math.exp(-distance / atomName.z1());
+		double exp2 = Math.exp(-distance / atomName.z2());
+		double exp3 = Math.exp(-distance / atomName.z3());
+		fac1 = atomName.cz1() * exp1 + atomName.cz2() * exp2 + atomName.cz3()
+				* exp3;// first derivative of density
+		fac2 = atomName.czz1() * exp1 + atomName.czz2() * exp2
+				+ atomName.czz3() * exp3;
+		density = getPromolecularDensity(atomName, exp1, exp2, exp3);
+		}
+		else{
+			
+
+			double f = 0.0;
+			double fp=0.0;
+		 double fpp=0.0;
+			RadialGrid grid = atom.getRadialGrid();
+			if (distance < grid.getPosition_max()) {
+
+				int ir = 0;
+				double r = 0;
+				double[] grid_positions = grid.getGrid_positions();
+				// careful with grid limits.
+				if (distance <= grid_positions[0]) {
+					
+					ir = 1;
+					r = grid_positions[0];
+				} else {
+					ir = (int) (1 + Math.floor(Math.log(distance / grid.getA())
+							/ grid.getB()));
+					r = distance;
+				}
+				
+				double[] rr = new double[4];
+				double[] dr1 = new double[4];
+				double[][] x1dr12 = new double[4][4];
+
+				for (int i = 0; i < 4; i++) {
+					int ii = (Math.min(Math.max(ir, 2), grid.getNgrid()) - 3 + i);
+					rr[i] = grid_positions[ii];
+					dr1[i] = r - rr[i];
+					
+					for (int j = 0; j <= i - 1; j++) {
+						x1dr12[i][j] = 1.0 / (rr[i] - rr[j]);
+						x1dr12[j][i] = -x1dr12[i][j];
+						
+					}
+				}
+				// interpolate, lagrange 3rd order, 4 nodes
+				for (int i = 0; i < 4; i++) {
+					int ii = (Math.min(Math.max(ir, 2), grid.getNgrid()) - 3 + i);
+					double prod = 1.0;
+					for (int j = 0; j < 4; j++) {
+						if (i == j) {
+							continue;
+						}
+						
+						prod = prod * dr1[j] * x1dr12[i][j];
+					}
+					// density=density+grid.getGrid_values()[ii]*prod;
+					f = f + grid.getGrid_values()[ii] * prod;
+					fp=fp+grid.getFirst_derivative_of_grid_values()[ii]*prod;
+					 fpp=fpp+grid.getSecond_derivative_of_grid_values()[ii]*prod;
+					
+				}
+			}// end of if distance
+			density=f;
+			fac1 =-fp;
+			fac2=fpp;		
+		}//end of grid density calculation
 		Vector gradientVector = getGradientVector(distanceUnitVector, fac1);
-		double density = getDensity(atomName, exp1, exp2, exp3);
+
 		Matrix hessian = getHessian(distanceVector, atomName,
-				distanceReciprocal, distanceUnitVector, exp1, exp2, exp3, fac1);
+				distanceReciprocal, distanceUnitVector, fac1, fac2);
 		// set densityPoint
 		densityPoint.setDensity(density);
 		densityPoint.setGradientVector(gradientVector);
@@ -97,23 +167,22 @@ public class AtomicDensityPropertiesCalculator implements
 		return distanceUnitVector.scalarMultiply(fac1 * -1.0);
 	}
 
-	private double getDensity(Element atomName, double exp1, double exp2,
+	private double getPromolecularDensity(Element atomName, double exp1, double exp2,
 			double exp3) {
 		return atomName.c1() * exp1 + atomName.c2() * exp2 + atomName.c3()
 				* exp3;
 	}
 
 	private Matrix getHessian(Vector distanceVector, Element atomName,
-			double distanceReciprocal, Vector distanceUnitVector, double exp1,
-			double exp2, double exp3, double fac1) {
-		Matrix hessian =  myMatrix.matrix3x3();
+			double distanceReciprocal, Vector distanceUnitVector, double fac1,
+			double fac2) {
+		Matrix hessian = myMatrix.matrix3x3();
 		Vector distanceUnitVector2 = distanceUnitVector
 				.ebeMultiply(distanceUnitVector);
 		double distanceReciprocal2 = distanceReciprocal * distanceReciprocal;
 		double fac11 = fac1 * distanceReciprocal;// first derivative of density
 													// divided by distance
-		double fac2 = atomName.czz1() * exp1 + atomName.czz2() * exp2
-				+ atomName.czz3() * exp3;
+
 		double fac3 = fac2 + fac11;// second derivative of density + fac11
 
 		// hessian is a 3x3 symmetrix matrix.get the upper right part at first.
--- yoink-core-density/src/test/groovy/org/wallerlab/yoink/density/service/densityProperties/AtomicDensityPropertiesCalculatorSpec.groovy
+++ yoink-core-density/src/test/groovy/org/wallerlab/yoink/density/service/densityProperties/AtomicDensityPropertiesCalculatorSpec.groovy
@@ -28,8 +28,10 @@ import org.wallerlab.yoink.density.domain.SimpleDensityPoint
 import org.wallerlab.yoink.density.service.densityProperties.AtomicDensityPropertiesCalculator
 import org.wallerlab.yoink.math.linear.CommonsVector3D
 import org.wallerlab.yoink.math.linear.SimpleVector3DFactory
+import org.wallerlab.yoink.molecular.data.RadialGridReader
 import org.wallerlab.yoink.molecular.domain.SimpleCoord
 import org.wallerlab.yoink.molecular.domain.SimpleCoordFactory
+import org.wallerlab.yoink.molecular.domain.SimpleRadialGrid
 import org.wallerlab.yoink.math.linear.SimpleMatrixFactory
 import spock.lang.Specification;
 
@@ -80,6 +82,78 @@ class AtomicDensityPropertiesCalculatorSpec extends Specification{
 		Math.abs(dv.getDensity()-0.40223E-03)<=1.0E-5
 		for(int i=0;i<3;i++){
 			Math.abs(dv.getGradientVector().getEntry(i)+0.0004391555)<=1.0E-5
+		}
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				Math.abs(dv.getHessian().data[i][j]-h[i][j])<=1.0E-5
+			}
+		}
+	}
+
+
+
+	def"calculate(densityValues,atom), check result values: density,gradientVector,hessian from radial grid"(){
+		given:
+		def myVector3D=new SimpleVector3DFactory()
+		myVector3D.myVectorType=Vector.Vector3DType.COMMONS
+		def myMatrix=new SimpleMatrixFactory()
+		myMatrix.matrixType=Matrix.Type.COMMONS
+		def simpleCoordFactory=new  SimpleCoordFactory()
+		simpleCoordFactory.myVector3D= myVector3D
+		def coordinate=  simpleCoordFactory.create((double[])[
+			10.149391801428232    ,
+			1.4386837495289542     ,
+			2.4831297529116525
+		])
+
+		def grid= new SimpleRadialGrid()
+		def reader= new RadialGridReader()
+
+		reader.myMatrix=myMatrix
+		reader.read("./src/test/resources/c__lda.wfc",  grid)
+		def aCoord=simpleCoordFactory.create((double[])[
+			10.448638218462468  ,
+			3.71765835974282866E-002 ,
+			1.0816225869801266
+		])
+
+		def a = Mock(Atom)
+		a.getElementType()>>Element.C
+		a.getRadialGrid()>>grid
+
+		a.getCoordinate()>>aCoord
+		double[][]	h=[
+			[
+				3.3309016742003245E-4,
+				5.526679305394191E-4,
+				5.526679305394191E-4
+			],
+			[
+				5.526679305394191E-4,
+				3.3309016742003245E-4,
+				5.526679305394191E-4
+			],
+			[
+				5.526679305394191E-4,
+				5.526679305394191E-4,
+				3.3309016742003245E-4
+			]
+		]
+		double[] 	grad=[
+			8.32535337485744778E-003 ,
+			-3.89914189430043967E-002,
+			-3.89914189430043967E-002
+		]
+		when:"make a new AtomicDensityPropertiesCalculator"
+		def calculator= new AtomicDensityPropertiesCalculator()
+		calculator.myMatrix=myMatrix
+		def dv=new SimpleDensityPoint(coordinate)
+
+		then:"assert gradient vector and hessian matrix"
+		calculator.calculate(dv, a)
+		Math.abs(dv.getDensity()-  2.74614659934315462E-002 )<=1.0E-4
+		for(int i=0;i<3;i++){
+			Math.abs(dv.getGradientVector().getEntry(i)-grad[i])<=1.0E-5
 		}
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
