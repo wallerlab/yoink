f5364cd92547e1c7db9e01ee43d474b7f63b3e77
Mark Waller <M_P_waller@hotmail.com>
2016-08-10 17:40:09 +0800

    Update SimpleMolecularSystem.java

--- yoink-core-molecule/src/main/java/org/wallerlab/yoink/molecule/domain/SimpleMolecularSystem.java
+++ yoink-core-molecule/src/main/java/org/wallerlab/yoink/molecule/domain/SimpleMolecularSystem.java
@@ -14,13 +14,12 @@
  * limitations under the License.
  */
 package org.wallerlab.yoink.molecule.domain;
+import org.wallerlab.yoink.api.model.molecular.MolecularSystem;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import org.wallerlab.yoink.api.model.molecular.MolecularSystem;
-
 import static java.util.stream.Collectors.toSet;
+
 /**
  * the domain model for molecular system.
  * 
@@ -29,40 +28,37 @@ import static java.util.stream.Collectors.toSet;
  */
 public class SimpleMolecularSystem implements MolecularSystem {
 
-	private final Set<Molecule> molecules;
+    private final Set<Molecule> molecules;
 
-	public SimpleMolecularSystem(Set<Molecule> molecules) {
-		this.molecules = molecules;
+    public SimpleMolecularSystem(Set<Molecule> molecules) {
+        this.molecules = molecules;
+    }
+
+    /**
+     * get all atoms in the molecule system.
+     */
+    @Override
+    public Set<Molecule.Atom> getAtoms() {
+        return molecules.stream()
+			.flatMap(molecule -> molecule.getAtoms()
+					   	     .stream())
+			.collect(toSet());
 	}
 
-	/**
-	 * get all atoms in the molecule system.
-	 */
-	@Override
-	public Set<Molecule.Atom> getAtoms() {
-		return molecules.stream()
-						.flatMap(molecule ->
-										molecule.getAtoms()
-												.stream())
-												.collect(toSet());
+    public Set<Molecule> getMolecules(String query){
+	return molecules.stream()
+			.filter(molecule -> molecule.getName()
+					            .toString()
+					           .equals(query))
+                        .collect(toSet());
 	}
 
-	public Set<Molecule> getMolecules(String query){
-		return molecules.stream()
-						.filter(molecule ->
-						           molecule.getName()
-								           .toString()
-								           .equals(query))
-						.collect(toSet());
-	}
-
-	/**
-	 * All molecules in molecular system.
-	 */
-	@Override
-	public Set<Molecule> getMolecules() {
-		return new HashSet<>(this.molecules);
-	}
-
+    /**
+     * All molecules in molecular system.
+     */
+    @Override
+    public Set<Molecule> getMolecules() {
+        return new HashSet<>(this.molecules);
+    }
 
 }
