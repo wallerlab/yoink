ef52e5218d83b7dae27201e8a4b51b43686a399f
Mark Waller <M_P_waller@hotmail.com>
2016-08-10 18:32:09 +0800

    Update SetOps.java

--- yoink-core-math/src/main/java/org/wallerlab/yoink/math/SetOps.java
+++ yoink-core-math/src/main/java/org/wallerlab/yoink/math/SetOps.java
@@ -5,7 +5,7 @@ import org.wallerlab.yoink.api.model.molecular.MolecularSystem;
 import java.util.*;
 
 /**
- * this class is to get the difference of two sets
+ * This class for generic operations on sets
  */
 public class SetOps {
 
@@ -79,33 +79,30 @@ public class SetOps {
         return result;
     }
 
-	/**
-	 * this method is to get all subsets with the same size of an integer array.
-	 * integer lists
-	 *
-	 * @param S
-	 *            - an integer array
-	 * @param size
-	 *            - the size of subsets
-	 *
-	 * @return if the input S is empty,return null else return an list of
-	 *         integer lists
-	 */
-	public static List<ArrayList<Integer>> split(int[] S, int size) {
-		if (S == null) return null;
-		Arrays.sort(S);
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		for (int i = 0; i < S.length; i++) {
-			List<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
-			// get sets that are already in result
-			for (ArrayList<Integer> a : result) temp.add(new ArrayList<Integer>(a));
-			// add S[i] to existing sets
-			for (ArrayList<Integer> a : temp) a.add(S[i]);
-			// add S[i] only as a set
-			ArrayList<Integer> single = new ArrayList<Integer>();
-			single.add(S[i]);
-			temp.add(single);
-			result.addAll(temp);
+    /**
+     * this method is to get all subsets with the same size of an integer array.
+     * integer lists
+     *
+     * @param S
+     *            - an integer array
+     * @param size
+     *            - the size of subsets
+     *
+     * @return if the input S is empty,return null
+     *         else return an list of integer lists
+     */
+    public static List<ArrayList<Integer>> split(int[] S, int size) {
+        if (S == null) return null;
+	    Arrays.sort(S);
+            List<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+	    for (int i = 0; i < S.length; i++) {
+	        List<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
+		for (ArrayList<Integer> a : result) temp.add(new ArrayList<Integer>(a));
+		for (ArrayList<Integer> a : temp) a.add(S[i]);
+		List<Integer> single = new ArrayList<Integer>();
+		single.add(S[i]);
+		temp.add(single);
+		result.addAll(temp);
 		}
 		List<ArrayList<Integer>> sizeResult = new ArrayList<ArrayList<Integer>>();
 		for(int i=0;i<result.size();i++)
@@ -114,32 +111,25 @@ public class SetOps {
 		return sizeResult;
 	}
 
-	/**
-	 *
-	 * @param S
-	 *            - an Double array
-	 * @return if the input S is empty,return null else return an list of
-	 *         Double lists
-	 */
+    /**
+     * @param S  - an Double array
+     * @return  return an list of  Double lists
+     */
     public static List<ArrayList<Double>> splitDoubleArray(double[] S) {
 	if (S == null) return null;
 	Arrays.sort(S);
 	List<ArrayList<Double>> result = new ArrayList<ArrayList<Double>>();
 	for (int i = 0; i < S.length; i++) {
     	    List<ArrayList<Double>> temp = new ArrayList<ArrayList<Double>>();
-	    // get sets that are already in result
-	    for (ArrayList<Double> a : result)
-	        temp.add(new ArrayList<Double>(a));
-	        // add S[i] to existing sets
+	    for (ArrayList<Double> a : result)  temp.add(new ArrayList<Double>(a));
 	        for (ArrayList<Double> a : temp) a.add(S[i]);
-    	        // add S[i] only as a set
 	        ArrayList<Double> single = new ArrayList<Double>();
 	        single.add(S[i]);
                 temp.add(single);
 	        result.addAll(temp);
 	}
-	// add empty set
 	result.add(new ArrayList<Double>());
 	return result;
     }
+    
 }
