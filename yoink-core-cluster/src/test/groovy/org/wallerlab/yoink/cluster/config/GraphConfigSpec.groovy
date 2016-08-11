4e72c0ec533f3dd70994f1fd8c968bff70e0dff7
mark waller <m.waller@uni-muenster.de>
2016-08-11 18:52:57 +0800

    fix test in cluster

---
+++ yoink-core-cluster/src/test/groovy/org/wallerlab/yoink/cluster/config/GraphConfigSpec.groovy
@@ -0,0 +1,24 @@
+package org.wallerlab.yoink.cluster.config
+
+import org.neo4j.ogm.session.SessionFactory
+import org.neo4j.ogm.transaction.TransactionManager
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager
+import org.wallerlab.yoink.cluster.GraphConfig
+import spock.lang.Specification
+
+/**
+ * Created by waller on 11/08/16.
+ */
+class GraphConfigSpec extends  Specification{
+
+    def "assert that the config is right"() {
+
+        when:
+        def graphConfig = new GraphConfig()
+
+        then:
+        graphConfig.sessionFactory()  instanceof  SessionFactory
+        graphConfig.transactionManager() instanceof  Neo4jTransactionManager
+
+    }
+}
