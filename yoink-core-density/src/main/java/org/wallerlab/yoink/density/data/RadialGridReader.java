aa68341524227539a9a422102b26546586622eb3
mark waller <m.waller@uni-muenster.de>
2016-08-12 20:53:24 +0800

    brittle

--- yoink-core-density/src/main/java/org/wallerlab/yoink/density/data/RadialGridReader.java
+++ yoink-core-density/src/main/java/org/wallerlab/yoink/density/data/RadialGridReader.java
@@ -78,12 +78,12 @@ public class RadialGridReader {
             {-50, 0, 225}};
 
 
-    private final String FILEPATH = "/Users/waller/merge/yoink/yoink-core-density/src/main/java/org/wallerlab/yoink/density/data/radialGrids.serialized";
+    private final String FILEPATH = "./src/main/java/org/wallerlab/yoink/density/data/radialGrids.serialized";
 
     public Map<Element,RadialGrid> read() throws IOException {
 
         //Save a lot of effort by reading in serialized version of the radial grids.
-        String serializedFilePath = "/Users/waller/merge/yoink/yoink-core-density/src/main/java/org/wallerlab/yoink/density/data/radialGrids.serialized";
+        String serializedFilePath = "./src/main/java/org/wallerlab/yoink/density/data/radialGrids.serialized";
         File file = new File(serializedFilePath);
         String current = new java.io.File( "." ).getCanonicalPath();
 
